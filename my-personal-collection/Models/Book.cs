c88cdfd1aeb52384836444b767ebdbacf3ca704c
Pedro Pires <ph_pires@poli.ufrj.br>
2020-02-11 23:27:42 -0300

    Commit inicial

---
+++ my-personal-collection/Models/Book.cs
@@ -0,0 +1,21 @@
+﻿using MyPersonalCollection.Api.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPersonalCollection.Api.Models
+{
+    public class Book : IEntity
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title;
+        [Required]
+        public string Author;
+        public int Edition;
+        public int? NumberOfPages;
+        public DateTime PublishedDate;
+    }
+}
